d150cc601052380b712bad209934c4d65959e39d
Mazdak Pakaghideh <me@mazdak.dev>
2023-10-03 12:43:09 +0330

    Add the base of StartPage

--- n-movies/src/views/StartPage.vue
+++ n-movies/src/views/StartPage.vue
@@ -5,23 +5,33 @@
           <ion-buttons slot="start">
             <ion-menu-button color="primary"></ion-menu-button>
           </ion-buttons>
-          <ion-title>Start</ion-title>
+          <ion-title>Let's Start Shall We.. ?</ion-title>
         </ion-toolbar>
       </ion-header>
-  
-      <ion-content :fullscreen="true">
-        <ion-header collapse="condense">
-          <ion-toolbar>
-            <ion-title size="large">{{ $route.params.id }}</ion-title>
-          </ion-toolbar>
-        </ion-header>
-  
-        <div id="container">
-          <strong class="capitalize">{{ $route.params.id }}</strong>
-          <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-        </div>
+
+      <ion-content class="ion-padding">
+        <h1>Choose</h1>
+        <ion-grid>
+            <ion-row justify-content-center align-items-center>
+                <ion-col size="6">
+                    <ion-button shape="round" fill="outline">Randomly</ion-button>
+                </ion-col>
+                <ion-col size="6">
+                    <ion-button shape="round" fill="outline">Manually</ion-button>
+                </ion-col>
+            </ion-row>
+        </ion-grid>
+
+        <h2>How To Choose</h2>
+        <h3 class="ml-3">Random Mode</h3>
+        <p class="ml-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque numquam eaque reprehenderit ullam rerum est perspiciatis aspernatur ipsum harum explicabo? In quidem nesciunt nam earum beatae dolores ipsam reprehenderit adipisci.</p>
+        <h3 class="ml-3">Manuall Mode</h3>
+        <p class="ml-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque numquam eaque reprehenderit ullam rerum est perspiciatis aspernatur ipsum harum explicabo? In quidem nesciunt nam earum beatae dolores ipsam reprehenderit adipisci.</p>
+         
       </ion-content>
+
     </ion-page>
+
   </template>
   
   <script setup lang="ts">
@@ -52,6 +62,14 @@
   
   #container a {
     text-decoration: none;
+  }
+
+  .ml-3{
+    margin-left: 0.75rem;
+  }
+
+  .ml-4{
+    margin-left: 1rem;
   }
   </style>
   
