231410903cb498333881ac897e0f0ca8284f8ad1
Mazdak Pakaghideh <me@mazdak.dev>
2023-10-04 13:35:28 +0330

    A change in modal

--- n-movies/src/views/StartPage.vue
+++ n-movies/src/views/StartPage.vue
@@ -24,36 +24,28 @@
                                 {{ movie.comment }}
                             </ion-card-content>
 
-                            <ion-button id="open-modal" fill="clear">افزودن <ion-icon class="mr-1" :icon="addCircleOutline"></ion-icon></ion-button>
+                            <ion-button @click="setOpen(true)" fill="clear">افزودن <ion-icon class="mr-1" :icon="addCircleOutline"></ion-icon></ion-button>
                         </ion-card>
                     </ion-col>
                 </ion-row>
             </ion-grid>
-
-            <ion-modal ref="modal" trigger="open-modal" @willDismiss="onWillDismiss">
+            <ion-modal :is-open="isOpen">
                 <ion-header>
                     <ion-toolbar>
-                    <ion-buttons slot="start">
-                        <ion-button @click="cancel()">لغو</ion-button>
-                    </ion-buttons>
                     <ion-title>اضافه کردن فیلم</ion-title>
                     <ion-buttons slot="end">
-                        <ion-button :strong="true" @click="confirm()">تایید</ion-button>
+                        <ion-button @click="setOpen(false)">لغو</ion-button>
                     </ion-buttons>
                     </ion-toolbar>
                 </ion-header>
+
                 <ion-content class="ion-padding">
                     <ion-item>
-                        <ion-label position="stacked">اسم فیلم را جستجو کنید</ion-label>
-                        <ion-input ref="input" type="text" placeholder="The Lord of the Rings"></ion-input>
-                    </ion-item>
-
-                    <ion-item class="mt-3">
-                        <ion-label position="stacked">نظرتان ؟</ion-label>
-                        <ion-textarea placeholder="به نظرم فیلم جالبی بود اما میتونستن تو انتخاب بازیگرا بیشتر دقت کنن" ></ion-textarea>
+                        <ion-label position="stacked">جستجوی فیلم</ion-label>
+                        <ion-input v-model="searchQuery" type="text" placeholder="The lord of the rings"></ion-input>
                     </ion-item>
                 </ion-content>
-             </ion-modal>
+        </ion-modal>
 
         </ion-content>
     </ion-page>
@@ -64,7 +56,7 @@
     import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonIcon} from '@ionic/vue';
     import { IonButton, IonModal, IonItem, IonInput, IonLabel, IonTextarea} from '@ionic/vue';
     import { addCircleOutline } from 'ionicons/icons';
-    import { ref } from 'vue';
+    import { ref, watch } from 'vue';
 
     const movies = ref(Array.from({length: 5}, (_, index) => ({
         id: index + 1,
@@ -74,22 +66,14 @@
         score: '0/0'
     })));
 
-    const modal = ref();
-  const input = ref();
+    const isOpen = ref(false);
+    const setOpen = (open: boolean) => (isOpen.value = open);
+    const searchQuery = ref('')
+    
+    watch(searchQuery, (newValue, oldValue) => {
+        
+    });
 
-  const cancel = () => modal.value.$el.dismiss(null, 'cancel');
-  const message = ref('This modal example uses triggers to automatically open a modal when the button is clicked.');
-
-  const confirm = () => {
-    const name = input.value.$el.value;
-    modal.value.$el.dismiss(name, 'confirm');
-  };
-
-  const onWillDismiss = (ev: CustomEvent<OverlayEventDetail>) => {
-    if (ev.detail.role === 'confirm') {
-      message.value = `Hello, ${ev.detail.data}!`;
-    }
-  };
 
 </script>
   
