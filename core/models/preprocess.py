55a96a3c532bae43792164c6a03c5c97000bd079
Mazdak Pakaghideh <me@mazdak.dev>
2023-10-18 17:26:12 +0330

    Fixed title splitter

--- core/models/preprocess.py
+++ core/models/preprocess.py
@@ -6,13 +6,12 @@ ratings_df = pandas.read_csv("ratings.csv")
 movies_df['year'] = movies_df.title.str.extract('(\(\d\d\d\d\))', expand=False)
 movies_df['year'] = movies_df.year.str.extract('(\d\d\d\d)', expand=False)
 movies_df['title'] = movies_df.title.str.replace('(\(\d\d\d\d\))', '', regex=True)
+movies_df['title'] = movies_df.title.str.replace('\([^()]*\)', '', regex=True) #Removing possible extra texts  
 movies_df['title'] = movies_df['title'].apply(lambda x: x.strip())
 movies_df = movies_df.drop('genres', axis=1)
-
-movies_df['title'] = movies_df['title'].apply(lambda x: x.split(",")[-1].strip())
-
-movies_df = movies_df.drop('genres', axis=1)
 ratings_df = ratings_df.drop('timestamp', axis=1)
+
+movies_df['title'] = movies_df['title'].apply(lambda x: " ".join(x.split(",")[::-1]).strip())
 
 movies_df.to_csv("movies.csv", index=False)
 ratings_df.to_csv("ratings.csv", index=False)
