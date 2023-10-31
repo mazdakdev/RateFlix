bb31ae3a9f398716cc5e41e7e3550f56c7efb579
Alireza Arabi <alirezaaraby5@gmail.com>
2023-10-31 03:07:28 -0700

    updated

--- rate-flix/src/views/HomePage.vue
+++ rate-flix/src/views/HomePage.vue
@@ -1,21 +1,87 @@
 <style scoped>
-  .example-content {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 100%;
+  .main {
+    overflow-x: scroll 
   }
+  .scrolling-wrapper {
+  overflow-x: scroll;
+  overflow-y: hidden;
+  white-space: nowrap;
+}
+.scrolling-wrapper .card {
+  display: inline-block;
+}
+
+.scrolling-wrapper-flexbox {
+  display: flex;
+  flex-wrap: nowrap;
+  overflow-x: auto;
+}
+.scrolling-wrapper-flexbox .card {
+  flex: 0 0 auto;
+  margin-right: 3px;
+}
+
+.card {
+  border: 2px solid #4a4848;
+  width: 150px;
+  height: 75px;
+  background: black;
+}
+
+.scrolling-wrapper, .scrolling-wrapper-flexbox {
+  height: 80px;
+  margin-bottom: 20px;
+  width: 100%;
+  -webkit-overflow-scrolling: touch;
+}
+.scrolling-wrapper::-webkit-scrollbar, .scrolling-wrapper-flexbox::-webkit-scrollbar {
+  display: none;
+}
 </style>
 
 <template>
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Listen now</ion-title>
+        <ion-title>خانه</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content>
-      <div class="example-content">Listen now content</div>
+      <div class="">
+        <div class="">
+        <ion-label>پیشنهادی</ion-label>
+          <div class="scrolling-wrapper">
+            <div class="card">
+              <h2>Card</h2>
+            </div>
+            <div class="card"><h2>Card</h2></div>
+            <div class="card"><h2>Card</h2></div>
+            <div class="card"><h2>Card</h2></div>
+            <div class="card"><h2>Card</h2></div>
+            <div class="card"><h2>Card</h2></div>
+            <div class="card"><h2>Card</h2></div>
+            <div class="card"><h2>Card</h2></div>
+            <div class="card"><h2>Card</h2></div>
+          </div>
+      </div>
+      </div>
+
+      <div class="">
+        <ion-label>داغ</ion-label>
+          <div class="scrolling-wrapper">
+            <div class="card">
+              <h2>Card</h2>
+            </div>
+            <div class="card"><h2>Card</h2></div>
+            <div class="card"><h2>Card</h2></div>
+            <div class="card"><h2>Card</h2></div>
+            <div class="card"><h2>Card</h2></div>
+            <div class="card"><h2>Card</h2></div>
+            <div class="card"><h2>Card</h2></div>
+            <div class="card"><h2>Card</h2></div>
+            <div class="card"><h2>Card</h2></div>
+          </div>
+      </div>
     </ion-content>
   </ion-page>
 </template>
--- rate-flix/src/views/ProfilePage.vue
+++ rate-flix/src/views/ProfilePage.vue
@@ -1,9 +1,30 @@
 <style scoped>
-  .center {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
+.flex-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.flex-row{
+  
+  flex-direction: row;
+}
+.flex-col{
+  flex-direction: column;
+  
+}
+.profile-image{
+  justify-content: right;
+  align-items: right;
+}
+
+.leble{
+  padding: 10px ;
+}
+
+.list{
+  margin-left: 20%;
+  margin-right: 20%;
+}
 </style>
 
 <template>
@@ -14,27 +35,57 @@
       </ion-toolbar>
     </ion-header>
 
-    <ion-content class="center">
+    <ion-content>
     
-      <div>
-      <ion-avatar>
-          <img alt="Silhouette of a person's head" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
+      <div class="flex-container flex-col">
+        <div class="flex-row">
+          <ion-avatar class="profile-image">
+            
+            <img alt="Silhouette of a person's head" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
+            
+          </ion-avatar>
+          <ion-label>علی علیزاده</ion-label>
+        </div>
 
-        </ion-avatar>
-        <ion-avatar>
-          <img alt="Silhouette of a person's head" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
+        <div class="flex-container flex-row">
+          <div>
+            <ion-label class="leble">دنبال کننده: 10 هزار نفر</ion-label>
+          </div>
+          <div>
 
-        </ion-avatar>
+            <ion-label class="leble">دنبال شونده: 100 نفر</ion-label>
+          </div>
+        </div>
+
       </div>
   
       <ion-segment value="default">
-    <ion-segment-button value="default">
-      <ion-label>حساب کاربری</ion-label>
-    </ion-segment-button>
-    <ion-segment-button value="segment">
-      <ion-label>اعلان ها</ion-label>
-    </ion-segment-button>
-  </ion-segment>
+
+        <ion-segment-button value="default">
+          <ion-label>حساب کاربری</ion-label>
+        </ion-segment-button>
+        
+        <ion-segment-button value="segment">
+          <ion-label>اعلان ها</ion-label>
+        </ion-segment-button>
+    </ion-segment>
+
+    
+    <ion-list :inset="true" class="list">
+      <ion-item :button="true" detail="false">
+      <ion-label>
+        <strong>تغییر رمز عبور</strong>
+      </ion-label>
+    </ion-item>
+        <ion-item>
+          <ion-toggle>
+            <ion-label>فعالسازی احراز هویت دو مرحله ای</ion-label>
+            <ion-note color="medium">حساب خود را ایمن کنید</ion-note>
+          </ion-toggle>
+        </ion-item>
+        
+
+      </ion-list>
 
     </ion-content>
   </ion-page>
