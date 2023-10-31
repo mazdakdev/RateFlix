44d0d78bac1a0f4ae947005622a46f36fae9870a
Alireza Arabi <alirezaaraby5@gmail.com>
2023-10-31 01:32:12 -0700

    Profile Updated

--- rate-flix/src/views/ProfilePage.vue
+++ rate-flix/src/views/ProfilePage.vue
@@ -1,9 +1,8 @@
 <style scoped>
-  .example-content {
+  .center {
     display: flex;
     align-items: center;
     justify-content: center;
-    height: 100%;
   }
 </style>
 
@@ -11,19 +10,42 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Profile</ion-title>
+        <ion-title>حساب کاربری</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content>
-      <div class="example-content">Profile content</div>
+
+    <ion-content class="center">
+    
+      <div>
+      <ion-avatar>
+          <img alt="Silhouette of a person's head" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
+
+        </ion-avatar>
+        <ion-avatar>
+          <img alt="Silhouette of a person's head" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
+
+        </ion-avatar>
+      </div>
+  
+      <ion-segment value="default">
+    <ion-segment-button value="default">
+      <ion-label>حساب کاربری</ion-label>
+    </ion-segment-button>
+    <ion-segment-button value="segment">
+      <ion-label>اعلان ها</ion-label>
+    </ion-segment-button>
+  </ion-segment>
+
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-  import { IonHeader, IonToolbar, IonTitle, IonContent, IonPage } from '@ionic/vue';
+  import { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonLabel, IonSegment, IonSegmentButton, IonAvatar } from '@ionic/vue';
+
 
   export default {
-    components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
+    components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonLabel, IonSegment, IonSegmentButton, IonAvatar },
   };
+
 </script>
--- rate-flix/src/views/SearchPage.vue
+++ rate-flix/src/views/SearchPage.vue
@@ -1,29 +1,19 @@
-<style scoped>
-  .example-content {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 100%;
-  }
-</style>
-
 <template>
-  <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Search</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content>
-      <div class="example-content">Search content</div>
-    </ion-content>
-  </ion-page>
+  <ion-header>
+    <ion-toolbar mdoe="ios">
+      <ion-title>Toolbar</ion-title>
+    </ion-toolbar>
+    <ion-toolbar>
+      <ion-searchbar></ion-searchbar>
+    </ion-toolbar>
+  </ion-header>
 </template>
 
 <script lang="ts">
-  import { IonHeader, IonToolbar, IonTitle, IonContent, IonPage } from '@ionic/vue';
+  import { IonHeader, IonSearchbar, IonTitle, IonToolbar } from '@ionic/vue';
+  import { defineComponent } from 'vue';
 
-  export default {
-    components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
-  };
+  export default defineComponent({
+    components: { IonHeader, IonSearchbar, IonTitle, IonToolbar },
+  });
 </script>
